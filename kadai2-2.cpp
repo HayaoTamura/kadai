542d5b88365cfa16d4c1c607627fe013650f33f6
HayaoTamura <58555009+HayaoTamura@users.noreply.github.com>
2020-01-24 20:37:33 +0900

    Update kadai2-2.cpp

--- kadai2-2.cpp
+++ kadai2-2.cpp
@@ -7,20 +7,21 @@ class gpio{
   //construct
   public:
   gpio();
-  
   private:
   int pi;
+  
   
   void cb_LED(const std_msgs::Bool::ConstPtr &data);
   ros::NodeHandle nh;
   ros::Subscriber sub_led;
 };
 
+
 gpio::gpio(){
   pi = pigpio_start(NULL,NULL);
-
   sub_led = nh.subscribe("/led", 5, &gpio::cb_LED, this);
 }
+
 
 void gpio::cb_LED(const std_msgs::Bool::ConstPtr &data){
   if(data->data == 1){
@@ -29,6 +30,7 @@ void gpio::cb_LED(const std_msgs::Bool::ConstPtr &data){
     gpio_write(pi,26,0);
   }
 }
+
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "gpio");
