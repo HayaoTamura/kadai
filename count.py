a7fab5dd193794fdaf53dcbf51ac53cb357fb7d3
HayaoTamura <58555009+HayaoTamura@users.noreply.github.com>
2020-01-28 14:53:37 +0900

    Update count.py

--- count.py
+++ count.py
@@ -6,8 +6,10 @@ rospy.init_node('count')
 pub = rospy.Publisher('count_up', Int32, queue_size=1)
 rate = rospy.Rate(1)
 n = 0
+p = 0
 while not rospy.is_shutdown():
     n += 1
     p += 1
     pub.publish(n)
+   #pub.publish(p)
     rate.sleep()
